a5b96a38e5bd06ea1dbe3c830e7368f5ed259260
Lance Clark <haYnguy@gmail.com>
2013-02-24 18:48:47 -0600

    Removed ivy.glade. Although it would have been to play around with something new, it didn't seem appropriate to continue down this path. Changes to Main.hs now allow for displaying an image in a simple interface.

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,18 @@
 module Main (main) where
 
 import Graphics.UI.Gtk
-import Graphics.UI.Gtk.Glade
 import System.Environment
 
 main :: IO ()
 main = do
-        args <- getArgs
-        showImage . head args 
-
-showImage :: String -> IO ()
-showImage []  = do putStrLn "Usage: ivy <image name>"
-showImage img = do
-                 initGUI 
-		 Just xml <- xmlNew "ivy.glade"
-		 guiWin   <- xmlGetWidget xml castToWindow "mainWindow"
-		 imgVw    <- xmlGetWidget xml castToImage  "imgW"
-		 onDestroy guiWin mainQuit
-		 widgetShow guiWin
-		 mainGUI
-
+	args <- getArgs
+	initGUI
+	guiWin <- windowNew
+	windowSetDefaultSize guiWin 600 400
+	scrWin <- scrolledWindowNew Nothing Nothing
+	imgVw  <- imageNewFromFile $ head args
+	scrolledWindowAddWithViewport scrWin imgVw
+	containerAdd guiWin scrWin
+	onDestroy guiWin mainQuit
+	widgetShowAll guiWin
+	mainGUI
