d40d87c3e936c60db929e9de97c4af2beeeb07ce
Lance Clark <haYnguy@gmail.com>
2013-02-25 21:23:25 -0600

    Added possible way to check commandline arguments

--- src/Main.hs
+++ src/Main.hs
@@ -6,13 +6,22 @@ import System.Environment
 main :: IO ()
 main = do
 	args <- getArgs
-	initGUI
-	guiWin <- windowNew
-	windowSetDefaultSize guiWin 600 400
-	scrWin <- scrolledWindowNew Nothing Nothing
-	imgVw  <- imageNewFromFile $ head args
-	scrolledWindowAddWithViewport scrWin imgVw
-	containerAdd guiWin scrWin
-	onDestroy guiWin mainQuit
-	widgetShowAll guiWin
-	mainGUI
+        showImage $ processArgs args
+
+processArgs :: [String] -> Maybe String
+processArgs (arg:[]) = Just arg
+processArgs _ = Nothing
+
+showImage :: Maybe String -> IO ()
+showImage Nothing = putStrLn "Usage: ivy <image_name>"
+showImage (Just imgFile) = do
+                        initGUI
+			guiWin <- windowNew
+			windowSetDefaultSize guiWin 600 400
+			scrWin <- scrolledWindowNew Nothing Nothing
+			imgVw  <- imageNewFromFile imgFile
+			scrolledWindowAddWithViewport scrWin imgVw
+			containerAdd guiWin scrWin
+			onDestroy guiWin mainQuit
+			widgetShowAll guiWin
+			mainGUI
